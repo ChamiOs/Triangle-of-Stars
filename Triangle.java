c5c1bdc8779106ee94fab07ef03301336b3784b3
Perunika Ilieva <admin12@abv.bg>
2017-04-18 11:28:24 +0300

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Triangle {
     public static void main(String[] args) {
-        String astriks = "*";
+        String asteriks = "*";
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
         for (int i = 1; i <= n; i++) {
-            System.out.println(astriks);
-            astriks = astriks + "*";
+            System.out.println(asteriks);
+            asteriks = asteriks + "*";
         }
 
     }
